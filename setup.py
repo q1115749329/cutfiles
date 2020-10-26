638d9baa0c88e0361a60231feb64e8b53c397950
zhenchaofeng <zhenchaofeng@tongyeyun.com>
2020-10-26 20:12:55 +0800

    更新

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@
 # @Author  : 甄超锋
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
+filepath = "README.md"
 setuptools.setup(
     name="cutfiles",
-    version="1.0.2",
+    version="1.0.5",
     author="甄超锋",
     author_email="4535@sohu.com",
     description="Cutting file tool.",
-    long_description=long_description,
+    long_description=open(filepath, encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/q1115749329/cutfiles",
     packages=setuptools.find_packages(),
+    data_files=[filepath],
+    include_package_data=True,
+    install_requires=["requests"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
